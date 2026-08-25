2df932117d31c284c2dda5f3e54a7c21e912805a
mrloganfredrick <loganfredrick22@gmail.com>
2026-08-25 17:22:51 +0300

    Change API_KEY to empty and update comment
    Updated API_KEY to be empty and added a comment to obtain a personal key.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const descriptionDisplay = document.getElementById("description");
   const errorMessage = document.getElementById("error-message");
 
-  const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"; //env variables
+  const API_KEY = ""; //get your own key
 
   getWeatherBtn.addEventListener("click", async () => {
     const city = cityInput.value.trim();
